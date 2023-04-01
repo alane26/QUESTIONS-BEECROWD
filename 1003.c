94d2f2643aeb36fd77483e6b1adde374cd85c3ef
alane26 <117764385+alane26@users.noreply.github.com>
2023-04-01 16:21:32 -0300

    Create 1003.c

---
+++ 1003.c
@@ -0,0 +1,19 @@
+/*
+  @autor: Alane;
+  @data: 01/04/2023;
+  @nome: Soma Simples;
+*/
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int soma = a + b;
+    printf("SOMA = %d\n", soma);
+ 
+    return 0;
+}
