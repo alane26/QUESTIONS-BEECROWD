a8c40eb0c9e75af325178f028752f3eadc03ad81
alane26 <117764385+alane26@users.noreply.github.com>
2023-04-01 16:37:55 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,21 @@
+/*
+  @autor: Alane;
+  @data: 01/04/2023;
+  @nome: Produto Simples;
+*/
+
+#include <stdio.h>
+
+int main(){
+    
+       int a;
+       scanf ("%d", &a);
+       
+       int b;
+       scanf ("%d", &b);
+       
+       int PROD = a * b;
+       printf ("PROD = %d\n", PROD);
+
+    return 0;
+}
