f0e12100a3cdd29e9e3536606bfa391621f87f6f
alane26 <117764385+alane26@users.noreply.github.com>
2023-04-01 16:36:33 -0300

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,18 @@
+/*
+  @autor: Alane;
+  @data: 01/04/2023;
+  @nome: Extremamente Básico;
+*/
+
+#include <stdio.h>
+ 
+int main() {
+    
+    int a, b;
+        scanf("%d%d", &a, &b);
+    
+    int x = a + b;
+        printf("X = %d\n", x);
+        
+    return 0;
+}
