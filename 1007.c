9006ceb8c708649b111977f5e3f8debd4cfed6be
alane26 <117764385+alane26@users.noreply.github.com>
2023-04-01 16:25:58 -0300

    Create 1007.c

---
+++ 1007.c
@@ -0,0 +1,20 @@
+/*
+  @autor: Alane;
+  @data: 01/04/2023;
+  @nome: Diferença;
+*/
+
+#include <stdio.h>
+ 
+int main() {
+    
+    int a, b, c, d;
+    
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        
+    int diferenca = (a * b - c * d);
+        
+        printf("DIFERENCA = %d\n", diferenca);
+        
+    return 0;
+}
